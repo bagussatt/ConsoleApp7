a4f593a19bdbe538d1173742ef5b2c24f471b6b2
Bagus Satrio <bagus.satrio.ft21@mail.umy.ac.id>
2022-12-02 16:54:52 +0700

    membuat class stack

--- ConsoleApp7/Program.cs
+++ ConsoleApp7/Program.cs
@@ -12,5 +12,48 @@ namespace ConsoleApp7
             next = n;
         }
     }
+    class Stacks
+    {
+        Node top;
+        public Stacks()
+        {
+            top = null;
+        }
+        bool empty()
+        {
+            if (top == null)
+                return (true);
+            else
+                return (false);
+        }
+        public void push(int element)
+        {
+            Node fresh;
+            fresh = new Node(element, null);
+            fresh.next = top;
+            top = fresh;
+            Console.WriteLine("\n" + element + " pushed.");
+        }
+        public void pop()
+        {
 
+            Console.WriteLine("\nThe popped element is: " + top.info);
+            top = top.next;
+        }
+        public void display()
+        {
+            Node tmp;
+            if (empty())
+                Console.WriteLine("\nStack Empty");
+            else
+            {
+                //Melakukan pertukaran
+                for (tmp = top; tmp != null; tmp = tmp.next)
+                {
+                    Console.WriteLine(tmp.info);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
 }
